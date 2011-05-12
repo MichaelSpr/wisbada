f8ae469428f164ac27ea534dd13acfec30e7303a
MichaelS <mail>
2011-05-12 19:26:46 +0200

    Cleanup (unnecessary files removed) empty index added

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Stammbaum</title>
+</head>
+<body>
+	<h1>noch nichts</h1>
+</body>
+</html>
