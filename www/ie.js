4a33f6def7666d2b1ffc197a3077d66468f1ab65
sdstoehr <sdstoehr@gmail.com>
2011-07-07 19:17:48 +0200

    IE<=8 now uses scrollbars (scaling not available)

--- www/ie.js
+++ www/ie.js
@@ -1,6 +1,12 @@
 jQuery(document).bind( "init_done", function() {
 	var $board = jQuery("#board");
 	var $main_ul = $board.children("ul");
-	$board.width( $main_ul.width() );
+	
+	var old_width = $board.width(); var new_width = $main_ul.width();
+	
+	if( new_width > old_width ) {
+		jQuery("#wrap").width( new_width );
+	}
+	
 	$main_ul.css("margin-left", 0);
 } );
